660374d386d5b1925e3b96f5662f0386a732ef56
Antony-LE <le.antony.l@gmail.com>
2022-02-12 18:52:07 +0100

    Ajout hook display list

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -14,15 +14,19 @@ import CardListDrink from '../CardListDrink/CardListDrink';
 function Main({ className }) {
   const [starterTitle, setStarterTitle] = useState('');
   const [starterImage, setStarterImage] = useState('');
+  const [displayStarterCardList, setDisplayStarterCardList] = useState(false);
 
   const [mainCourseTitle, setMainCourseTitle] = useState('');
   const [mainCourseImage, setMainCourseImage] = useState('');
+  const [displayMainCourseCardList, setDisplayMainCourseCardList] = useState(false);
 
   const [dessertTitle, setDessertTitle] = useState('');
   const [dessertImage, setDessertImage] = useState('');
+  const [displayDessertCardList, setDisplayDessertCardList] = useState(false);
 
   const [drinkTitle, setDrinkTitle] = useState('');
   const [drinkImage, setDrinkImage] = useState('');
+  const [displayDrinkCardList, setDisplayDrinkCardList] = useState(false);
 
   const apiKey = 'c0d15de7e0bd47eeb023b186b7c521a1';
   const baseUrlStarter = `https://api.spoonacular.com/recipes/random?number=1&type=soup,salad&apiKey=${apiKey}`;
@@ -34,6 +38,7 @@ function Main({ className }) {
     await axios.get(baseUrlStarter).then((response) => {
       setStarterTitle(response.data.recipes[0].title);
       setStarterImage(response.data.recipes[0].image);
+      setDisplayStarterCardList(!displayStarterCardList);
     });
   };
 
@@ -41,6 +46,7 @@ function Main({ className }) {
     await axios.get(baseUrlMainCourse).then((response) => {
       setMainCourseTitle(response.data.recipes[0].title);
       setMainCourseImage(response.data.recipes[0].image);
+      setDisplayMainCourseCardList(!displayMainCourseCardList);
     });
   };
 
@@ -48,13 +54,15 @@ function Main({ className }) {
     await axios.get(baseUrlDessert).then((response) => {
       setDessertTitle(response.data.recipes[0].title);
       setDessertImage(response.data.recipes[0].image);
+      setDisplayDessertCardList(!displayDessertCardList);
     });
   };
 
   const handleClickButtonDrink = async () => {
     await axios.get(baseUrlDrink).then((response) => {
-      setDessertTitle(response.data.recipes[0].title);
-      setDessertImage(response.data.recipes[0].image);
+      setDrinkTitle(response.data.recipes[0].title);
+      setDrinkImage(response.data.recipes[0].image);
+      setDisplayDrinkCardList(!displayDrinkCardList);
     });
   };
 
@@ -64,10 +72,10 @@ function Main({ className }) {
       <ButtonMainCourse className="button-main-course" handleRandomMainCourse={handleClickButtonMainCourse} />
       <ButtonDessert className="button-dessert" handleRandomDessert={handleClickButtonDessert} />
       <ButtonDrink className="button-drink" handleRandomDrink={handleClickButtonDrink} />
-      <CardListStarter className="cardlist-starter" handleStarterImage={starterImage} handleStarterTitle={starterTitle} />
-      <CardListMainCourse className="cardlist-main-course" handleMainCourseImage={mainCourseImage} handleMainCourseTitle={mainCourseTitle} />
-      <CardListDessert className="cardlist-dessert" handleDessertImage={dessertImage} handleDessertTitle={dessertTitle} />
-      <CardListDrink className="cardlist-drink" handleDrinkImage={drinkImage} handleDrinkTitle={drinkTitle} />
+      {displayStarterCardList ? <CardListStarter className="cardlist-starter" handleStarterImage={starterImage} handleStarterTitle={starterTitle} /> : '' }
+      {displayMainCourseCardList ? <CardListMainCourse className="cardlist-main-course" handleMainCourseImage={mainCourseImage} handleMainCourseTitle={mainCourseTitle} /> : '' }
+      {displayDessertCardList ? <CardListDessert className="cardlist-dessert" handleDessertImage={dessertImage} handleDessertTitle={dessertTitle} /> : '' }
+      {displayDrinkCardList ? <CardListDrink className="cardlist-drink" handleDrinkImage={drinkImage} handleDrinkTitle={drinkTitle} /> : '' }
     </main>
   );
 }
